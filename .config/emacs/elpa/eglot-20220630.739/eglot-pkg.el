f66f04cc0099bfa164693ada0169718696ac4f23
Francesco Vellutini <vellufrance@gmail.com>
2022-07-07 16:33:42 +0200

    Update

---
+++ .config/emacs/elpa/eglot-20220630.739/eglot-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from eglot.el  -*- no-byte-compile: t -*-
+(define-package "eglot" "20220630.739" "Client for Language Server Protocol (LSP) servers" '((emacs "26.1") (jsonrpc "1.0.14") (flymake "1.2.1") (project "0.3.0") (xref "1.0.1") (eldoc "1.11.0") (seq "2.23")) :commit "bd970be047aba4513c2970b9ff2d842f0472cb0e" :authors '(("João Távora" . "joaotavora@gmail.com")) :maintainer '("João Távora" . "joaotavora@gmail.com") :keywords '("convenience" "languages") :url "https://github.com/joaotavora/eglot")
